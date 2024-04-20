01b0376de36fa4914705ee82417f6c4c5c0b6ed1
Francesco Sodano <francesco-sodano@github.com>
2024-04-21 00:50:52 +0200

    Fixed bug in install-vscode-extension not detecting correctly the VS Code.

--- Tasks/install-vscode-extension/install-vscode-extension.ps1
+++ Tasks/install-vscode-extension/install-vscode-extension.ps1
@@ -19,22 +19,6 @@ Set-DevBoxCustomizationVariables
 # Main Script----------------------------------- #
 # ---------------------------------------------- #
 
-$codecommand = "code"
-$codeinsidercommand = "code-insiders"
-
-if (Get-Command -Name $codecommand -ErrorAction SilentlyContinue) {
-    Write-Output "Visual Studio Code detected."
-    $Command = "$codecommand --install-extension $Extension"
-} 
-elseif (Get-Command -Name $code$codeinsidercommand -ErrorAction SilentlyContinue){
-    Write-Output "Visual Studio Code Insiders detected."
-    $Command = "$codeinsidercommand --install-extension $Extension"
-}
-else {
-    Write-Output "VS Code or VS Code Insiders NOT detected."
-    exit 1
-}
-
 # We're running as user via scheduled task:
 if ($RunAsUser -eq "true") {
     Write-Host "Running as user via scheduled task"
@@ -64,7 +48,21 @@ if ($RunAsUser -eq "true") {
         Merge-DevBoxCustomizationUserScript "Write-Host 'Powershell 7 Installed'"
         # Install the VS Code Extension
         Merge-DevBoxCustomizationUserScript "Write-Host 'Installing Visual Studio Code Extension: ' $($Extension)"
-        Merge-DevBoxCustomizationUserScript "$($Command)"
+        # Check if Visual Studio Code or Visual Studio Code insiders is installed
+        Merge-DevBoxCustomizationUserScript "if (Get-Command -Name 'code' -ErrorAction SilentlyContinue) {"
+        Merge-DevBoxCustomizationUserScript "   Write-Output 'Visual Studio Code detected.'"
+        # Install the VS Code Extension with VS Code
+        Merge-DevBoxCustomizationUserScript "   code --install-extension $($Extension)"
+        Merge-DevBoxCustomizationUserScript "}" 
+        Merge-DevBoxCustomizationUserScript "elseif (Get-Command -Name 'code-insiders' -ErrorAction SilentlyContinue) {"
+        Merge-DevBoxCustomizationUserScript "   Write-Output 'Visual Studio Code Insiders detected.'"
+        # Install the VS Code Extension with VS Code Insiders
+        Merge-DevBoxCustomizationUserScript "   code-insiders --install-extension $($Extension)"
+        Merge-DevBoxCustomizationUserScript "}"
+        Merge-DevBoxCustomizationUserScript "else {"
+        # Write a message if VS Code or VS Code Insiders is not detected
+        Merge-DevBoxCustomizationUserScript "   Write-Output 'VS Code or VS Code Insiders NOT detected.'"
+        Merge-DevBoxCustomizationUserScript "}"
         # Update the PATH environment variable
         Merge-DevBoxCustomizationUserScript "Write-host 'Updating PATH'"
         Merge-DevBoxCustomizationUserScript '$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")'
